c7fb2841f6e61ec481cd21e2ac6d226b3cd3c2db
traintechno1 <admin@Chaitalis-MacBook-Air.local>
2024-12-27 09:03:18 +0530

    Rest api with static data

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const app = express();
+const creditCards = require('./static-data/credit-cards');
 
-app.get('/getMessage', (req, res)=>{
+app.get('/message', (req, res)=>{
     res.send("Good Morning!!");
+});
+
+app.get('/credit-card', (req, res)=>{
+    let cc = {
+        cardNumber: 896812017129,
+        expiryMonth: 4,
+        expiryYear: 2027,
+        cvv: 234,
+        holderName: 'Jalim Chakma'
+    }; 
+    res.json(cc);
+})
+
+app.get('/credit-cards', (req, res)=>{
+    res.json(creditCards);
+})
+
+app.get('/payload', (req, res)=>{
+    res.json({
+        status: 200,
+        message: 'Api called successfuly',
+        data: {
+            id: 100,
+            city: 'pune'
+        }
+    })
 })
 
 // the below method creates a server with base URL: http://localhost:4000
---
+++ middleware/static-data/credit-cards.js
@@ -0,0 +1,46 @@
+const creditCards = [
+    {
+        cardNumber: 896812017129,
+        expiryMonth: 4,
+        expiryYear: 2027,
+        cvv: 234,
+        holderName: 'Jalim Chakma'
+    },
+    {
+        cardNumber: 988721293092,
+        expiryMonth: 6,
+        expiryYear: 2030,
+        cvv: 762,
+        holderName: 'Jalim Chakma'
+    },
+    {
+        cardNumber: 876541904634,
+        expiryMonth: 7,
+        expiryYear: 2025,
+        cvv: 754,
+        holderName: 'Gurveer Singh'
+    },
+    {
+        cardNumber: 986758633567,
+        expiryMonth: 4,
+        expiryYear: 2026,
+        cvv: 234,
+        holderName: 'Gurveer Singh'
+    },
+    {
+        cardNumber: 9867281391821,
+        expiryMonth: 4,
+        expiryYear: 2027,
+        cvv: 988,
+        holderName: 'Dipika Sugandh'
+    },
+    {
+        cardNumber: 896712836956,
+        expiryMonth: 3,
+        expiryYear: 2028,
+        cvv: 776,
+        holderName: 'Dipika Sugandh'
+    },
+];
+
+module.exports = {creditCards};
